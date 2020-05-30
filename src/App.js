399d1b979de3ac9b7388ddaa50d5f15a3110832a
rossig <rrossigabriel@gmail.com>
2020-05-30 18:22:00 -0300

    Adds withStyle and converts default app style from css to js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import { TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+import styles from './AppStyle';
 
 const GET_REVIEWS = gql`
   query GET_REVIEWS {
@@ -14,41 +16,39 @@ const GET_REVIEWS = gql`
   }
 `;
 
-function App() {
+function App(props) {
+  const { classes } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Query query={GET_REVIEWS}>
-            {({ error, data, loading }) => {
-              if( error ) {
-                console.log("er", error);
-                return null;
-              }
-              if( loading ) return null;
-              return (
-                <>
-                  {data.reviews.map((review, i) => (
-                    <div key={i}>
-                      <p>{review.title}</p>
-                      <p>{review.text}</p>
-                    </div>
-                  ))}
-                </>
-              )
-            }}
-          </Query>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.root}>
+      <header className={classes.header}>
+        <img src={logo} className={classes.logo} alt="logo" />
+        <Query query={GET_REVIEWS}>
+          {({ error, data, loading }) => {
+            if( error ) {
+              console.log("er", error);
+              return null;
+            }
+            if( loading ) return null;
+            return (
+              <>
+                {data.reviews.map((review, i) => (
+                  <div key={i}>
+                    <p>{review.title}</p>
+                    <p>{review.text}</p>
+                  </div>
+                ))}
+              </>
+            )
+          }}
+        </Query>
+        <form noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Standard" />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/AppStyle.js
@@ -0,0 +1,20 @@
+export default {
+  root: {
+    textAlign: "center"
+  },
+  logo: {
+    height: "40vmin"
+  },
+  header: {
+    backgroundColor: "#393d45",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+    paddingLeft: "20vw",
+    paddingRight: "20vw"
+  }
+}
