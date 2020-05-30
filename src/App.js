f0733467540b80bf82e699795c57d4490f9ce332
rossig <rrossigabriel@gmail.com>
2020-05-30 08:14:20 -0300

    Adds a query to the main page that shows a list of reviews

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_REVIEWS = gql`
+  query GET_REVIEWS {
+    reviews{
+      id
+      title
+      text
+    }
+  }
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Query query={GET_REVIEWS}>
+            {({ error, data, loading }) => {
+              if( error ) {
+                console.log("er", error);
+                return null;
+              }
+              if( loading ) return null;
+              return (
+                <>
+                  {data.reviews.map((review, i) => (
+                    <div key={i}>
+                      <p>{review.title}</p>
+                      <p>{review.text}</p>
+                    </div>
+                  ))}
+                </>
+              )
+            }}
+          </Query>
         <a
           className="App-link"
           href="https://reactjs.org"
